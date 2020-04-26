1d6e165e86463e23064da0a783cabba55f4cdd44
akram-bakenov <56001306+akram-bakenov@users.noreply.github.com>
2020-04-26 22:13:46 +0600

    Update hsv_filter.py

--- hsv_filter.py
+++ hsv_filter.py
@@ -1,11 +1,15 @@
 import numpy as np
 import cv2
 
+
 def nothing(x):
     pass
 
-cv2.namedWindow( "trackbars" )   # window for trackbars
-cv2.namedWindow( "webcam" ) # webcam window
+# window for trackbars
+cv2.namedWindow("trackbars") 
+
+# webcam window
+cv2.namedWindow("webcam")    
 
 cap = cv2.VideoCapture(0)     # webcam capture
 
@@ -16,12 +20,13 @@ cv2.createTrackbar('h2', 'trackbars', 255, 255, nothing)
 cv2.createTrackbar('s2', 'trackbars', 255, 255, nothing)
 cv2.createTrackbar('v2', 'trackbars', 255, 255, nothing)
 
-
-while (cap.isOpened()):  # while true
+# while true
+while (cap.isOpened()):  
     _, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #convert
+    # convertion
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
 
-    # считываем значения бегунков
+    # read trackbar's value
     h1 = cv2.getTrackbarPos('h1', 'trackbars')
     s1 = cv2.getTrackbarPos('s1', 'trackbars')
     v1 = cv2.getTrackbarPos('v1', 'trackbars')
@@ -29,11 +34,11 @@ while (cap.isOpened()):  # while true
     s2 = cv2.getTrackbarPos('s2', 'trackbars')
     v2 = cv2.getTrackbarPos('v2', 'trackbars')
 
-    # формируем начальный и конечный цвет фильтра
+    # inital and final filter's colour 
     h_min = np.array((h1, s1, v1), np.uint8)
     h_max = np.array((h2, s2, v2), np.uint8)
 
-    # накладываем фильтр на кадр в модели HSV
+    # put filter to the frame 
     thresh = cv2.inRange(hsv, h_min, h_max)
 
     res = cv2.bitwise_and(frame, frame, mask=thresh)
@@ -45,4 +50,5 @@ while (cap.isOpened()):  # while true
         break
 
 cv2.destroyAllWindows()
-cap.release() # erases cap
+# erases cap
+cap.release() 
